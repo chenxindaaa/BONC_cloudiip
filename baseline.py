bdad3592a00e488567aab485a33fc9b11ad1e163
chenxindaaa <948900213@qq.com>
2019-12-31 11:37:50 +0800

    测试1

--- baseline.py
+++ baseline.py
@@ -5,144 +5,148 @@ import matplotlib.pyplot as plt
 import math
 
 
+class Apparatus:
+    def __init__(self, name):
+        self.angle = []
+        self.src = cv.imread(name)
 
-def line_detect_possible_demo(image, center):
-    res = {}
-    edges = cv.Canny(image, 50, 150, apertureSize=3)
-    cv.imshow("abcdefg", edges)
-    lines = cv.HoughLinesP(edges, 1, np.pi/180, 13, minLineLength=20, maxLineGap=5)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        #k = cv.waitKey(10000)
+    def line_detect_possible_demo(self, image, center):
+        res = {}
+        edges = cv.Canny(image, 50, 150, apertureSize=5)
+        cv.imshow("abcdefg", edges)
+        lines = cv.HoughLinesP(edges, 1, np.pi/180, 13, minLineLength=20, maxLineGap=5)
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            #k = cv.waitKey(10000)
 
-        # 将坐标原点移动到圆心
-        x1 -= center[0]
-        y1 = center[1] - y1
-        x2 -= center[0]
-        y2 = center[1] - y2
-        # 计算斜率
-        k = (y2-y1)/(x2-x1)
-        d1 = np.sqrt(max(abs(x2), abs(x1)) ** 2 + (max(abs(y2), abs(y1))) ** 2)  # 线段长度
-        d2 = np.sqrt(min(abs(x2), abs(x1)) ** 2 + (min(abs(y2), abs(y1))) ** 2)
-        if d1 < 155 and d1 > 148 and d2 > 115:
-            res[k] = [1]
-        elif d1 < 110 and d1 > 100 and d2 > 75:
-            res[k] = [2]
+            # 将坐标原点移动到圆心
+            x1 -= center[0]
+            y1 = center[1] - y1
+            x2 -= center[0]
+            y2 = center[1] - y2
+            # 计算斜率
+            k = (y2-y1)/(x2-x1)
+            d1 = np.sqrt(max(abs(x2), abs(x1)) ** 2 + (max(abs(y2), abs(y1))) ** 2)  # 线段长度
+            d2 = np.sqrt(min(abs(x2), abs(x1)) ** 2 + (min(abs(y2), abs(y1))) ** 2)
+            if d1 < 155 and d1 > 148 and d2 > 115:
+                res[k] = [1]
+            elif d1 < 110 and d1 > 100 and d2 > 75:
+                res[k] = [2]
+            else:
+                continue
+            res[k].append(1) if (x2 + x1) /2 > 0 else res[k].append(0)  # 将14象限与23象限分离
+            cv.line(self.src, (x1 + center[0], center[1] - y1), (x2 + center[0],  center[1] - y2), (255, 0, 0), 1)
+            #t = cv.waitKey(10000)
+            print(x1 + center[0], center[1] - y1, x2 + center[0], center[1] - y2)
+
+            cv.imshow("line_detect-posssible_demo", self.src)
+            print('k = %f' % k)
+            print('d1 = %f' % d1)
+            print('d2 = %f' % d2)
+        angle1 = [i for i in res if res[i][0] == 1]
+        angle2 = [i for i in res if res[i][0] == 2]
+
+        a = np.arctan(angle1[0])
+        b = np.arctan(angle1[1])
+        if a * b < 0 and abs(a) > np.pi / 4:
+           if a + b < 0:
+               self.angle.append(math.degrees(-(a + b) / 2)) if res[angle1[1]][1] == 1 else self.angle.append(
+                   math.degrees(-(a + b) / 2) + 180)
+           else:
+               self.angle.append(math.degrees(np.pi - (a + b) / 2)) if res[angle1[1]][1] == 1 else self.angle.append(
+                   math.degrees(np.pi - (a + b) / 2) + 180)
         else:
-            continue
-        res[k].append(1) if (x2 + x1) /2 > 0 else res[k].append(0)  # 将14象限与23象限分离
-        cv.line(src, (x1 + center[0], center[1] - y1), (x2 + center[0],  center[1] - y2), (255, 0, 0), 1)
-        #t = cv.waitKey(10000)
-        print(x1 + center[0], center[1] - y1, x2 + center[0], center[1] - y2)
-
-        cv.imshow("line_detect-posssible_demo", src)
-        print('k = %f' % k)
-        print('d1 = %f' % d1)
-        print('d2 = %f' % d2)
-    angle1 = [i for i in res if res[i][0] == 1]
-    angle2 = [i for i in res if res[i][0] == 2]
-    angle = []
-
-    a = np.arctan(angle1[0])
-    b = np.arctan(angle1[1])
-    if a * b < 0 and abs(a) > np.pi / 4:
-       if a + b < 0:
-           angle.append(math.degrees(-(a + b) / 2)) if res[angle1[1]][1] == 1 else angle.append(
-               math.degrees(-(a + b) / 2) + 180)
-       else:
-           angle.append(math.degrees(np.pi - (a + b) / 2)) if res[angle1[1]][1] == 1 else angle.append(
-               math.degrees(np.pi - (a + b) / 2) + 180)
-    else:
-        angle.append(math.degrees(np.pi / 2 - (a + b) / 2)) if res[angle1[1]][1] == 1 else angle.append(math.degrees(np.pi / 2 - (a + b) / 2) + 180)
-    print('长指针读数：%f' % angle[0])
-    # print(math.degrees(a - b))
+            self.angle.append(math.degrees(np.pi / 2 - (a + b) / 2)) if res[angle1[1]][1] == 1 else self.angle.append(math.degrees(np.pi / 2 - (a + b) / 2) + 180)
+        print('长指针读数：%f' % self.angle[0])
+        # print(math.degrees(a - b))
 
 
 
-    a = np.arctan(angle2[0])
-    b = np.arctan(angle2[1])
-    if a * b < 0 and abs(a) > np.pi / 4:
-       if a + b < 0:
-           angle.append(math.degrees(-(a + b) / 2)) if res[angle1[1]][1] == 1 else angle.append(
-               math.degrees(-(a + b) / 2) + 180)
-       else:
-           angle.append(math.degrees(np.pi - (a + b) / 2)) if res[angle1[1]][1] == 1 else angle.append(
-               math.degrees(np.pi - (a + b) / 2) + 180)
-    else:
-        angle.append(math.degrees(np.pi / 2 - (a + b) / 2)) if res[angle2[1]][1] == 1 else angle.append(math.degrees(np.pi / 2 - (a + b) / 2) + 180)
-    print('短指针读数：%f' % angle[1])
-    # print(math.degrees(a - b))
+        a = np.arctan(angle2[0])
+        b = np.arctan(angle2[1])
+        if a * b < 0 and abs(a) > np.pi / 4:
+           if a + b < 0:
+               self.angle.append(math.degrees(-(a + b) / 2)) if res[angle1[1]][1] == 1 else self.angle.append(
+                   math.degrees(-(a + b) / 2) + 180)
+           else:
+               self.angle.append(math.degrees(np.pi - (a + b) / 2)) if res[angle1[1]][1] == 1 else self.angle.append(
+                   math.degrees(np.pi - (a + b) / 2) + 180)
+        else:
+            self.angle.append(math.degrees(np.pi / 2 - (a + b) / 2)) if res[angle2[1]][1] == 1 else self.angle.append(math.degrees(np.pi / 2 - (a + b) / 2) + 180)
+        print('短指针读数：%f' % self.angle[1])
+        # print(math.degrees(a - b))
 
 
 
-def get_center(mask):
-    edg_output = cv.Canny(mask, 66, 150, 2)
-    cv.imshow('edg', edg_output)
-    # 外接矩形
-    contours, hireachy = cv.findContours(edg_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    center = []
-    for i, contour in enumerate(contours):
-        x, y, w, h = cv.boundingRect(contour)  # 外接矩形
-        area = w * h  # 面积
-        if area > 1000 or area < 40:
-            continue
-        print(area)
-        # cv.circle(src, (np.int(cx), np.int(cy)), 3, (255), -1)
-        cv.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 1)
-        cx = w / 2
-        cy = h / 2
-        cv.circle(src, (np.int(x + cx), np.int(y + cy)), 1, (255, 0, 0))
-        center.extend([np.int(x + cx), np.int(y + cy)])
-        break
+    def get_center(self, mask):
+        edg_output = cv.Canny(mask, 66, 150, 2)
+        cv.imshow('edg', edg_output)
+        # 外接矩形
+        contours, hireachy = cv.findContours(edg_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        center = []
+        for i, contour in enumerate(contours):
+            x, y, w, h = cv.boundingRect(contour)  # 外接矩形
+            area = w * h  # 面积
+            if area > 1000 or area < 40:
+                continue
+            print(area)
+            # cv.circle(src, (np.int(cx), np.int(cy)), 3, (255), -1)
+            cv.rectangle(self.src, (x, y), (x + w, y + h), (255, 0, 0), 1)
+            cx = w / 2
+            cy = h / 2
+            cv.circle(self.src, (np.int(x + cx), np.int(y + cy)), 1, (255, 0, 0))
+            center.extend([np.int(x + cx), np.int(y + cy)])
+            break
 
-    cv.imshow('final', src)
-    return center
-
-
-def corner_detect(src, gray):
-    dst = cv.cornerHarris(gray, 3, 27, 0.04)
-    print(dst.shape)  # (400, 600)
-    src[dst > 0.01 * dst.max()] = [0, 255, 255]
-    cv.imshow('corner', src)
+        # cv.imshow('final', src)
+        return center
 
 
+    def corner_detect(self, src, gray):
+        dst = cv.cornerHarris(gray, 3, 27, 0.04)
+        print(dst.shape)  # (400, 600)
+        src[dst > 0.01 * dst.max()] = [0, 255, 255]
+        # cv.imshow('corner', src)
 
-# 读取图片
-src = cv.imread('1_0107.jpg')
-# 缩小图片
-src = cv.resize(src, dsize=None, fx=0.5, fy=0.5)  # 此处可以修改插值方式interpolation
-cv.imshow('source', src)
+    def extract(self, image):
+        red_lower1 = np.array([0, 43, 46])
+        red_lower2 = np.array([156, 43, 46])
+        red_upper1 = np.array([10, 255, 255])
+        red_upper2 = np.array([180, 255, 255])
+        frame = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        mask1 = cv.inRange(frame, lowerb=red_lower1, upperb=red_upper1)
+        mask2 = cv.inRange(frame, lowerb=red_lower2, upperb=red_upper2)
+        mask = cv.add(mask1, mask2)
+        mask = cv.bitwise_not(mask)
+        cv.imshow('mask', mask)
+        return mask
 
-# 均值迁移
-blur = cv.pyrMeanShiftFiltering(src, 10, 16)
-cv.imshow('blur', blur)
+    def test(self):
+        # 缩小图片
+        self.src = cv.resize(self.src, dsize=None, fx=0.5, fy=0.5)  # 此处可以修改插值方式interpolation
+        cv.imshow('source', self.src)
 
+        # 均值迁移
+        blur = cv.pyrMeanShiftFiltering(self.src, 10, 16)
+        # cv.imshow('blur', blur)
 
-# 提取红色
-red_lower1 = np.array([0, 43, 46])
-red_lower2 = np.array([156, 43, 46])
-red_upper1 = np.array([10, 255, 255])
-red_upper2 = np.array([180, 255, 255])
-frame = cv.cvtColor(blur,cv.COLOR_BGR2HSV)
-mask1 = cv.inRange(frame, lowerb=red_lower1, upperb=red_upper1)
-mask2 = cv.inRange(frame, lowerb=red_lower2, upperb=red_upper2)
-mask = cv.add(mask1, mask2)
-mask = cv.bitwise_not(mask)
-cv.imshow('mask', mask)
-center = get_center(mask)
-line_detect_possible_demo(mask, center)
-#corner_detect(src, mask)
+        # 提取红色
+        mask = self.extract(blur)
+        center = self.get_center(mask)
+        self.line_detect_possible_demo(mask, center)
+        # corner_detect(src, mask)
 
 
 
 
 
-
-
-
-k = cv.waitKey(0)
-if k == 27:
-    cv.destroyAllWindows()
-elif k == ord('s'):
-    cv.imwrite('new.jpg', src)
-    cv.destroyAllWindows()
+if __name__ == '__main__':
+    apparatus = Apparatus('1_0305.jpg')
+    # 读取图片
+    apparatus.test()
+    k = cv.waitKey(0)
+    if k == 27:
+        cv.destroyAllWindows()
+    elif k == ord('s'):
+        cv.imwrite('new.jpg', apparatus.src)
+        cv.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,48 +1,20 @@
-import cv2  # 本来想用mpl的，但是cv2功能很强大，就用cv2了
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-
-img = np.zeros((64, 64, 3), np.uint8)  # 生成一个空彩色图像
+import baseline
+import os
 
 
-class Node():  # 构造结点类
-    num_nodes = 0
-
-    def __init__(self, name, postion1x, postion1y, width, height):
-        self.name = name  # 名字
-        self.position1x = postion1x  # 中心的水平方向坐标
-        self.position1y = postion1y  # 垂直方向
-        self.width = width  # 宽度
-        self.height = height  # 高度
-
-    def movement(obj):
-        return np.sqrt(np.sum((obj.position1 - obj.position2) ** 2))  # 移动距离
-
-    def displayNode(self):
-        cv2.rectangle(img, (int(self.position1x - self.width / 2), int(self.position1y - self.height / 2)),
-                      (int(self.position1x + self.width / 2), int(self.position1y + self.height / 2)), (55, 255, 155),
-                      1)  # 给出左上角端点和右下角端点就可以画出矩形了
-# 用cv2绘制出矩形
-
-node1 = Node("A", 20, 45, 30, 15)
-node1.displayNode()
-node2 = Node("B", 35, 40, 20, 45)
-node2.displayNode()
-node3 = Node("C", 40, 25, 20, 10)
-node3.displayNode()
-node4 = Node("D", 55, 57, 20, 10)
-node4.displayNode()
-img = np.uint8(img)
-img = cv2.resize(img, dsize=None, fx=5, fy=5)
-cv2.imshow('brg', img)  # 把画布展示出来
-
-k = cv2.waitKey(0)
-if k == 27:
-    cv2.destroyAllWindows()
-elif k == ord('s'):
-    cv2.imwrite('new.jpg', img)
-    cv2.destroyAllWindows()
+data_path = './BONC cloudiip工业仪表表盘读数大赛/'
+data = os.listdir(data_path)
+f = open('result.txt', 'w')
+for i in data:
+    apparatus = baseline.Apparatus(data_path + i)
+    res = apparatus.angle
+    try:
+        apparatus.test()
+        f.write(i + '\t长指针读数： %f 短指针读数： %f \n' % (res[0], res[1]))
+    except IndexError as reason:
+        f.write(i + str(reason) + '\n')
+        continue
 
 
-print(math.degrees(np.arctan(-1)))
+
+f.close()
